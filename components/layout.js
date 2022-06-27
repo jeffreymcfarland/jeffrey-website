607075aa5ff8a23ffc4f844aaf5d9d9147be7303
Jeffrey McFarland <jeffrey.mcfarland@ramseysolutions.com>
2022-06-27 16:33:47 -0500

    add metadata

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,17 @@
+import Head from "next/head";
 import Navbar from "./navbar";
 import Footer from "./footer";
 
 export default function Layout({ children }) {
   return (
     <>
+      <Head>
+        <meta
+          name="description"
+          content="A personal website for Jeffrey McFarland"
+        />
+        <meta property="og:image" content="/site-image.png" />
+      </Head>
       <Navbar />
       <main tabIndex={0} style={{ flexGrow: 1 }}>
         {children}
